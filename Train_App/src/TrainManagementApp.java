0570260733a033ee660ee8607fa7943ecf9ea69a
inshalashraf <mdinshalashraf@gmail.com>
2026-09-12 16:32:05 +0530

    Add clean train management entry point

---
+++ Train_App/src/TrainManagementApp.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class TrainManagementApp {
+    public static void main(String[] args) {
+        System.out.println("======================================");
+        System.out.println("Train Management - Ordered Consist");
+        System.out.println("======================================\n");
+
+        List<String> trainConsist = new LinkedList<>();
+        trainConsist.add("Bogie-1");
+        trainConsist.add("Bogie-2");
+        trainConsist.add("Bogie-3");
+
+        System.out.println("Initial Train Consist:");
+        System.out.println(trainConsist);
+
+        trainConsist.add(1, "Bogie-1A");
+        System.out.println("\nAfter inserting Bogie-1A at position 1:");
+        System.out.println(trainConsist);
+
+        trainConsist.remove(0);
+        trainConsist.remove(trainConsist.size() - 1);
+        System.out.println("\nAfter removing front and rear bogies:");
+        System.out.println(trainConsist);
+    }
+}
